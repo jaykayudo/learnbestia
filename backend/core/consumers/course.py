94ec088d87f6a30da69781aac5515d58ba1d86c0
jaykayudo <jaykayudo@gmail.com>
2025-11-04 22:03:18 +0100

    feat: create service and event handler for course websocket

--- backend/core/consumers/course.py
+++ backend/core/consumers/course.py
@@ -1,16 +1,19 @@
 from typing import Literal
 
+from pydantic import BaseModel, ValidationError
+
+
 from channels.db import database_sync_to_async
 from channels.exceptions import StopConsumer
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from loguru import logger
-from pydantic import BaseModel
 
 from core.models import course as course_models
 from core.utils import get_course_channel_name
+from core.consumers.service import EVENT_HANDLERS
 
 
-class CourseEvent(BaseModel):
+class Event(BaseModel):
     type: Literal[
         "chat",
         "announcement",
@@ -44,7 +47,46 @@ class CourseConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
 
     async def send_message(self, event):
+        """
+        event format:
+         - type: "question","announcement"
+         - data: question_data or announcement_data
+        e.g question_data:
+            - id: uuid as str
+            - course: uuid as str
+            - subject: str
+            - description: str
+        e.g announcement_data:
+            - id: uuid as str
+            - poster: uuid as str
+            - subject: str
+            - message: str
+        """
         await self.send_json(event["data"])
+
+    async def receive_json(self, content, **kwargs):
+        """
+        The format of the received content will be;
+        content: {
+            'type': str
+            'data': dict
+        }
+        """
+        type = content["type"]
+        data: dict = content["data"]
+        # validate data received from room
+        try:
+            event = Event(type=type, data=data)
+            user = self.user
+            data = {"user": user, **data}
+            if event.type in EVENT_HANDLERS:
+                response = EVENT_HANDLERS[event.type].process_data(
+                    self.course_id, event.data
+                )
+                if response:
+                    await self.send_json(response)
+        except ValidationError:
+            await self.send_json({"type": "error", "data": "Error validating request"})
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.course_name, self.channel_name)
