5f19bec0444d42a7b02bee69b7d96726cd28705e
jaykayudo <jaykayudo@gmail.com>
2025-11-11 13:20:51 +0100

    feat: create views for wallet features

--- backend/instructor/urls.py
+++ backend/instructor/urls.py
@@ -10,4 +10,24 @@ urlpatterns = [
         views.DashboardAPIView.as_view(),
         name="dashboard",
     ),
+    path(
+        "wallet/balance/",
+        views.WalletBalanceAPIView.as_view(),
+        name="wallet-balance",
+    ),
+    path(
+        "wallet/transactions/",
+        views.WalletTransactionListAPIView.as_view(),
+        name="wallet-transactions",
+    ),
+    path(
+        "wallet/withdrawal-requests/",
+        views.WithdrawalRequestListAPIView.as_view(),
+        name="withdrawal-requests",
+    ),
+    path(
+        "wallet/withdrawal-requests/create/",
+        views.WithdrawalRequestCreateAPIView.as_view(),
+        name="withdrawal-request-create",
+    ),
 ]
